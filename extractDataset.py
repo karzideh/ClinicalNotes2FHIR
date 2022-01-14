1b0a15f031fe0e0c511d710f5ab0bbc819edbd49
karzideh <kamyarzideh@web.de>
2022-01-14 10:41:16 +0100

    Replace input-directory with dummy

--- extractDataset.py
+++ extractDataset.py
@@ -1,3 +1,4 @@
 import ctakes_parser as parser
 
-parser.ctakes_parser.parse_dir("C:/Users/kamya/IdeaProjects/cTakes-api/src/main/resources/echtdaten/annotations/training/", "dataset/cTakes/patients/training")
+# Input directory should point to cTakes generared XMI-files
+parser.ctakes_parser.parse_dir("inputDir", "dataset/cTakes/patients/training")
